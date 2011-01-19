3e1c0d3793e55e37bd0e510136b18de22c2b183b
Jonathan Wright <quaggy@gmail.com>
2011-01-20 12:33:41 +1300

    Update so that it can create an .obj for a single chunk
    Very simple

--- ntb.go
+++ ntb.go
@@ -22,6 +22,10 @@ const (
     tagStruct = 10 // { A sequential list of Named Tags. This array keeps going until a TAG_End is found.; TAG_End end } Notes: If there's a nested TAG_Compound within this tag, that one will also have a TAG_End, so simply reading until the next TAG_End will not work. The names of the named tags have to be unique within each TAG_Compound The order of the tags is not guaranteed.
 )
 
+const (
+    trace = false
+)
+
 func main() {
     var file, fileErr = os.Open("c.4.4.dat", os.O_RDONLY, 0666) // x: 76, y: 69
     if fileErr != nil {
@@ -45,48 +49,56 @@ func main() {
 
         switch typeId {
         case tagStruct:
-            fmt.Printf("%s struct start\n", name)
+            if trace {
+                fmt.Printf("%s struct start\n", name)
+            }
         case tagStructEnd:
-            fmt.Printf("struct end\n")
+            if trace {
+                fmt.Printf("struct end\n")
+            }
         case tagByteArray:
             var bytes, err2 = readBytes(br)
             if err2 != nil {
                 fmt.Println(err2)
             }
-            fmt.Printf("%s bytes(%d) %v\n", name, len(bytes), bytes)
+            if trace {
+                fmt.Printf("%s bytes(%d) %v\n", name, len(bytes), bytes)
+            }
             if name == "Blocks" {
-                for i := 0; i < len(bytes); i += 128 {
-                    var column = bytes[i:i+128]
-                    for _, blockId := range column {
-                        fmt.Printf("%2x", blockId)
-                    }
-                    fmt.Println()
-                }
+                processBlocks(bytes)
             }
         case tagInt8:
             var number, err2 = readInt8(br)
             if err2 != nil {
                 fmt.Println(err2)
             }
-            fmt.Printf("%s int8 %v\n", name, number)
+            if trace {
+                fmt.Printf("%s int8 %v\n", name, number)
+            }
         case tagInt16:
             var number, err2 = readInt16(br)
             if err2 != nil {
                 fmt.Println(err2)
             }
-            fmt.Printf("%s int16 %v\n", name, number)
+            if trace {
+                fmt.Printf("%s int16 %v\n", name, number)
+            }
         case tagInt32:
             var number, err2 = readInt32(br)
             if err2 != nil {
                 fmt.Println(err2)
             }
-            fmt.Printf("%s int32 %v\n", name, number)
+            if trace {
+                fmt.Printf("%s int32 %v\n", name, number)
+            }
         case tagInt64:
             var number, err2 = readInt64(br)
             if err2 != nil {
                 fmt.Println(err2)
             }
-            fmt.Printf("%s int64 %v\n", name, number)
+            if trace {
+                fmt.Printf("%s int64 %v\n", name, number)
+            }
         case tagList:
             var itemTypeId, length, err2 = readListHeader(br)
             if err2 != nil {
@@ -94,13 +106,17 @@ func main() {
             }
             switch itemTypeId {
                 case tagInt8:
-                    fmt.Printf("%s list int8 (%d)\n", name, length)
+                    if trace {
+                        fmt.Printf("%s list int8 (%d)\n", name, length)
+                    }
                     for i := 0; i < length; i++ {
                         var v, err3 = readInt8(br)
                         if err3 != nil {
                             fmt.Println(err3)
                         }
-                        fmt.Println("  ", v)
+                        if trace {
+                            fmt.Println("  ", v)
+                        }
                     }
                 default:
                     fmt.Printf("list todo(%n) %n\n", itemTypeId, length)
@@ -187,4 +203,85 @@ func readIntN(r *bufio.Reader, n int) (int, os.Error) {
     }
 
     return a, nil
+}
+
+type Blocks []byte
+
+func (b *Blocks) Get(x, y, z int) byte {
+    return (*b)[y + (z*128 + (x * 128 * 16))]
+}
+
+func (b *Blocks) IsEmpty(x, y, z int) bool {
+    if x < 0 || y < 0 || z < 0 || x >= 16 || y >= 16 || z >= 16 {
+        return true
+    }
+    return isEmptyBlockId(b.Get(x, y, z))
+}
+
+func isEmptyBlockId(blockId byte) bool {
+    return blockId == 0 || blockId == 1
+}
+
+func processBlocks(bytes []byte) {
+    var blocks = Blocks(bytes)
+
+    for i := 0; i < len(blocks); i += 128 {
+        var x, z = (i / 128) / 16, (i / 128) % 16
+
+        var column = blocks[i:i+128]
+        fmt.Printf("# %x, %x:\n", x, z)
+        for y, blockId := range column {
+            if !isEmptyBlockId(blockId) {
+                if blocks.IsEmpty(x, y-1, z) {
+                    printFace(v{x, y, z}, v{x+1, y, z}, v{x+1, y, z+1}, v{x, y, z+1})
+                }
+
+                if blocks.IsEmpty(x, y+1, z) {
+                    printFace(v{x, y+1, z}, v{x, y+1, z+1}, v{x+1, y+1, z+1}, v{x+1, y+1, z})
+                }
+
+                if blocks.IsEmpty(x-1, y, z) {
+                    printFace(v{x, y, z}, v{x, y, z+1}, v{x, y+1, z+1}, v{x, y+1, z})
+                }
+
+                if blocks.IsEmpty(x+1, y, z) {
+                    printFace(v{x+1, y, z}, v{x+1, y+1, z}, v{x+1, y+1, z+1}, v{x+1, y, z+1})
+                }
+
+                if blocks.IsEmpty(x, y, z-1) {
+                    printFace(v{x, y, z}, v{x, y+1, z}, v{x+1, y+1, z}, v{x+1, y, z})
+                }
+
+                if blocks.IsEmpty(x, y, z+1) {
+                    printFace(v{x, y, z+1}, v{x+1, y, z+1}, v{x+1, y+1, z+1}, v{x, y+1, z+1})
+                }
+            }
+        }
+        fmt.Println()
+    }
+}
+
+func printVertex(x, y, z int) {
+    fmt.Printf("v %v %v %v\n", float64(x)*0.05, float64(y)*0.05, float64(z)*0.05)
+}
+
+func printFaceN(vertexes... int) {
+    fmt.Printf("f")
+    for _, vertex := range vertexes {
+        fmt.Printf(" %v", vertex)
+    }
+    fmt.Println()
+}
+
+type v struct { x, y, z int }
+
+func printFace(vertexes... v) {
+    for _, vertex := range vertexes {
+        printVertex(vertex.x, vertex.y, vertex.z)
+    }
+    fmt.Printf("f")
+    for i, _ := range vertexes {
+        fmt.Printf(" -%d", len(vertexes) - i)
+    }
+    fmt.Println()
 }

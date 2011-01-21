0f451088ae5739655a1026c774b35ac4f831daf8
Jonathan Wright <quaggy@gmail.com>
2011-01-21 19:42:10 +1300

    Add water transparency and option to show tunnels

--- ntb.go
+++ ntb.go
@@ -29,26 +29,28 @@ const (
 )
 
 var (
-	out        *bufio.Writer
-	faces      *Faces
-	yMin       int
-	blockFaces bool
+	out         *bufio.Writer
+	faces       *Faces
+	yMin        int
+	blockFaces  bool
+	showTunnels bool
 )
 
 func main() {
 	var filename string
 	flag.IntVar(&yMin, "y", 0, "Omit all blocks below this height. 63 is sea level")
 	flag.BoolVar(&blockFaces, "bf", false, "Don't combine adjacent faces of the same block within a column")
+	flag.BoolVar(&showTunnels, "t", false, "Show tunnels")
 	flag.StringVar(&filename, "o", "a.obj", "Name for output file")
 	flag.Parse()
 
 	if flag.NArg() != 0 {
 		var mtlFilename = fmt.Sprintf("%s.mtl", filename[:len(filename)-len(path.Ext(filename))])
 		var mtlErr = writeMtlFile(mtlFilename)
-        if mtlErr != nil {
-            fmt.Fprintln(os.Stderr, mtlErr)
-            return
-        }
+		if mtlErr != nil {
+			fmt.Fprintln(os.Stderr, mtlErr)
+			return
+		}
 
 		var outFile, outErr = os.Open(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
 		if outErr != nil {
@@ -291,6 +293,8 @@ func processChunk(filename string) bool {
 	}
 	fmt.Fprintln(out)
 
+	out.Flush()
+
 	return abort
 }
 
@@ -386,16 +390,20 @@ func (b *Blocks) IsEmpty(x, y, z int) bool {
 
 func (b *Blocks) IsBoundary(x, y, z int, blockId byte) bool {
 	if x < 0 || y < 0 || z < 0 || x >= 16 || y >= 128 || z >= 16 {
-		return blockId != 0
+		if showTunnels {
+			return blockId == 18 /* leaves */ || blockId == 17 /* wood */
+		} else {
+			return blockId != 0 && blockId != 9
+		}
 	}
 
 	var otherBlockId = b.Get(x, y, z)
 
-	if blockId == 1 && otherBlockId == 0 {
+	if blockId == 9 && otherBlockId == 0 {
 		return true
 	}
 
-	return (blockId != 0 && blockId != 1) && (otherBlockId == 0 || blockId == 1)
+	return (blockId != 0 && blockId != 9) && (otherBlockId == 0 || otherBlockId == 9)
 }
 
 func isEmptyBlockId(blockId byte) bool {
@@ -542,130 +550,131 @@ func printMtl(w io.Writer, blockId byte) {
 }
 
 func writeMtlFile(filename string) os.Error {
-    var outFile, outErr = os.Open(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
-    if outErr != nil {
-        return outErr
-    }
-    defer outFile.Close()
+	var outFile, outErr = os.Open(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if outErr != nil {
+		return outErr
+	}
+	defer outFile.Close()
 
-    var p = 0
+	var p = 0
 
-    for i := 0; i < 256; i++ {
-        var mtl *MTL
-        if p < len(colors) && colors[p].blockId == byte(i) {
-            mtl = &colors[p]
-            p++
-        } else {
-            mtl = &MTL{byte(i), 0x7f7f7f, "Unknown"}
-        }
+	for i := 0; i < 256; i++ {
+		var mtl *MTL
+		if p < len(colors) && colors[p].blockId == byte(i) {
+			mtl = &colors[p]
+			p++
+		} else {
+			mtl = &MTL{byte(i), 0x7f7f7f, "Unknown"}
+		}
 
-        mtl.Print(outFile)
-    }
+		mtl.Print(outFile)
+	}
 
-    return nil
+	return nil
 }
 
 type MTL struct {
 	blockId byte
-	kd      int
+	color   uint32
 	name    string
 }
 
 func (mtl *MTL) Print(w io.Writer) {
-    var (
-        r = mtl.kd >> 16
-        g = mtl.kd >> 8 & 0xff
-        b = mtl.kd & 0xff
-    )
+	var (
+		r = mtl.color >> 24
+		g = mtl.color >> 16 & 0xff
+		b = mtl.color >> 8 & 0xff
+		a = mtl.color & 0xff
+	)
 
-    fmt.Fprintf(w, "# %s\nnewmtl %d\nKd %.4f %.4f %.4f\n\n", mtl.name, mtl.blockId, float64(r) / 255, float64(g) / 255, float64(b) / 255)
+	fmt.Fprintf(w, "# %s\nnewmtl %d\nKd %.4f %.4f %.4f\nd %.4f\nillum 1\n\n", mtl.name, mtl.blockId, float64(r)/255, float64(g)/255, float64(b)/255, float64(a)/255)
 }
 
 var (
 	colors = []MTL{
-		MTL{0, 0xff0000, "Air"},
-		MTL{1, 0x757575, "Stone"},
-		MTL{2, 0x2e6d05, "Grass"},
-		MTL{3, 0x593d29, "Dirt"},
-		MTL{4, 0x3d3d3d, "Cobblestone"},
-		MTL{5, 0x9f844d, "Wooden Plank"},
-		MTL{6, 0x7f7f7f, "Sapling"},
-		MTL{7, 0x070707, "Bedrock"},
-		MTL{8, 0x3d6dff, "Water"},
-		MTL{9, 0x3d6dff, "Stationary water"},
-		MTL{10, 0x7f7f7f, "Lava"},
-		MTL{11, 0x7f7f7f, "Stationary lava"},
-		MTL{12, 0xbfb882, "Sand"},
-		MTL{13, 0x7f7f7f, "Gravel"},
-		MTL{14, 0x7f7f7f, "Gold ore"},
-		MTL{15, 0x7f7f7f, "Iron ore"},
-		MTL{16, 0x7f7f7f, "Coal ore"},
-		MTL{17, 0x675231, "Wood"},
-		MTL{18, 0x509026, "Leaves"},
-		MTL{19, 0x7f7f7f, "Sponge"},
-		MTL{20, 0x7f7f7f, "Glass"},
-		MTL{21, 0x7f7f7f, "Lapis Lazuli Ore"},
-		MTL{22, 0x7f7f7f, "Lapis Lazuli Block"},
-		MTL{23, 0x7f7f7f, "Dispenser"},
-		MTL{24, 0xbfb882, "Sandstone"},
-		MTL{25, 0x7f7f7f, "Note Block"},
-		MTL{35, 0x7f7f7f, "Wool D B"},
-		MTL{37, 0x7f7f7f, "Yellow flower"},
-		MTL{38, 0x7f7f7f, "Red rose"},
-		MTL{39, 0x7f7f7f, "Brown Mushroom"},
-		MTL{40, 0x7f7f7f, "Red Mushroom"},
-		MTL{41, 0x7f7f7f, "Gold Block"},
-		MTL{42, 0x7f7f7f, "Iron Block"},
-		MTL{43, 0x7f7f7f, "Double Stone Slab"},
-		MTL{44, 0x7f7f7f, "Stone Slab"},
-		MTL{45, 0x7f7f7f, "Brick"},
-		MTL{46, 0x7f7f7f, "TNT"},
-		MTL{47, 0x7f7f7f, "Bookshelf"},
-		MTL{48, 0x7f7f7f, "Moss Stone"},
-		MTL{49, 0x7f7f7f, "Obsidian"},
-		MTL{50, 0x7f7f7f, "Torch"},
-		MTL{51, 0x7f7f7f, "Fire"},
-		MTL{52, 0x7f7f7f, "Monster Spawner"},
-		MTL{53, 0x7f7f7f, "Wooden Stairs"},
-		MTL{54, 0x7f7f7f, "Chest"},
-		MTL{55, 0x7f7f7f, "Redstone Wire"},
-		MTL{56, 0x7f7f7f, "Diamond Ore"},
-		MTL{57, 0x7f7f7f, "Diamond Block"},
-		MTL{58, 0x7f7f7f, "Workbench"},
-		MTL{59, 0x7f7f7f, "Crops"},
-		MTL{60, 0x7f7f7f, "Soil"},
-		MTL{61, 0x7f7f7f, "Furnace"},
-		MTL{62, 0x7f7f7f, "Burning Furnace"},
-		MTL{63, 0x7f7f7f, "Sign Post"},
-		MTL{64, 0x7f7f7f, "Wooden Door"},
-		MTL{65, 0x7f7f7f, "Ladder"},
-		MTL{66, 0x7f7f7f, "Minecart Tracks"},
-		MTL{67, 0x7f7f7f, "Cobblestone Stairs"},
-		MTL{68, 0x7f7f7f, "Wall Sign"},
-		MTL{69, 0x7f7f7f, "Lever"},
-		MTL{70, 0x7f7f7f, "Stone Pressure Plate"},
-		MTL{71, 0x7f7f7f, "Iron Door"},
-		MTL{72, 0x7f7f7f, "Wooden Pressure Plate"},
-		MTL{73, 0x7f7f7f, "Redstone Ore"},
-		MTL{74, 0x7f7f7f, "Glowing Redstone Ore"},
-		MTL{75, 0x7f7f7f, "Redstone torch (\"off\" state)"},
-		MTL{76, 0x7f7f7f, "Redstone torch (\"on\" state)"},
-		MTL{77, 0x7f7f7f, "Stone Button"},
-		MTL{78, 0x000000, "Snow"},
-		MTL{79, 0x70a0ff, "Ice"},
-		MTL{80, 0x000000, "Snow Block"},
-		MTL{81, 0x7f7f7f, "Cactus"},
-		MTL{82, 0x7f7f7f, "Clay"},
-		MTL{83, 0x7f7f7f, "Sugar Cane"},
-		MTL{84, 0x7f7f7f, "Jukebox"},
-		MTL{85, 0x7f7f7f, "Fence"},
-		MTL{86, 0x7f7f7f, "Pumpkin"},
-		MTL{87, 0x7f7f7f, "Netherrack"},
-		MTL{88, 0x7f7f7f, "Soul Sand"},
-		MTL{89, 0x7f7f7f, "Glowstone"},
-		MTL{90, 0x7f7f7f, "Portal"},
-		MTL{91, 0x7f7f7f, "Jack-O-Lantern"},
-		MTL{92, 0xffffff, "Cake Block"},
+		MTL{0, 0xff0000ff, "Air"},
+		MTL{1, 0x757575ff, "Stone"},
+		MTL{2, 0x2e6d05ff, "Grass"},
+		MTL{3, 0x593d29ff, "Dirt"},
+		MTL{4, 0x3d3d3dff, "Cobblestone"},
+		MTL{5, 0x9f844dff, "Wooden Plank"},
+		MTL{6, 0x7f7f7fff, "Sapling"},
+		MTL{7, 0x070707ff, "Bedrock"},
+		MTL{8, 0x3d6dff7f, "Water"},
+		MTL{9, 0x3d6dff7f, "Stationary water"},
+		MTL{10, 0x7f7f7fff, "Lava"},
+		MTL{11, 0x7f7f7fff, "Stationary lava"},
+		MTL{12, 0xbfb882ff, "Sand"},
+		MTL{13, 0x7f7f7fff, "Gravel"},
+		MTL{14, 0x7f7f7fff, "Gold ore"},
+		MTL{15, 0x7f7f7fff, "Iron ore"},
+		MTL{16, 0x7f7f7fff, "Coal ore"},
+		MTL{17, 0x675231ff, "Wood"},
+		MTL{18, 0x509026ff, "Leaves"},
+		MTL{19, 0x7f7f7fff, "Sponge"},
+		MTL{20, 0x7f7f7fff, "Glass"},
+		MTL{21, 0x7f7f7fff, "Lapis Lazuli Ore"},
+		MTL{22, 0x7f7f7fff, "Lapis Lazuli Block"},
+		MTL{23, 0x7f7f7fff, "Dispenser"},
+		MTL{24, 0xbfb882ff, "Sandstone"},
+		MTL{25, 0x7f7f7fff, "Note Block"},
+		MTL{35, 0x7f7f7fff, "Wool D B"},
+		MTL{37, 0x7f7f7fff, "Yellow flower"},
+		MTL{38, 0x7f7f7fff, "Red rose"},
+		MTL{39, 0x7f7f7fff, "Brown Mushroom"},
+		MTL{40, 0x7f7f7fff, "Red Mushroom"},
+		MTL{41, 0x7f7f7fff, "Gold Block"},
+		MTL{42, 0x7f7f7fff, "Iron Block"},
+		MTL{43, 0x7f7f7fff, "Double Stone Slab"},
+		MTL{44, 0x7f7f7fff, "Stone Slab"},
+		MTL{45, 0x7f7f7fff, "Brick"},
+		MTL{46, 0x7f7f7fff, "TNT"},
+		MTL{47, 0x7f7f7fff, "Bookshelf"},
+		MTL{48, 0x7f7f7fff, "Moss Stone"},
+		MTL{49, 0x7f7f7fff, "Obsidian"},
+		MTL{50, 0x7f7f7fff, "Torch"},
+		MTL{51, 0x7f7f7fff, "Fire"},
+		MTL{52, 0x7f7f7fff, "Monster Spawner"},
+		MTL{53, 0x7f7f7fff, "Wooden Stairs"},
+		MTL{54, 0x7f7f7fff, "Chest"},
+		MTL{55, 0x7f7f7fff, "Redstone Wire"},
+		MTL{56, 0x7f7f7fff, "Diamond Ore"},
+		MTL{57, 0x7f7f7fff, "Diamond Block"},
+		MTL{58, 0x7f7f7fff, "Workbench"},
+		MTL{59, 0x7f7f7fff, "Crops"},
+		MTL{60, 0x7f7f7fff, "Soil"},
+		MTL{61, 0x7f7f7fff, "Furnace"},
+		MTL{62, 0x7f7f7fff, "Burning Furnace"},
+		MTL{63, 0x7f7f7fff, "Sign Post"},
+		MTL{64, 0x7f7f7fff, "Wooden Door"},
+		MTL{65, 0x7f7f7fff, "Ladder"},
+		MTL{66, 0x7f7f7fff, "Minecart Tracks"},
+		MTL{67, 0x7f7f7fff, "Cobblestone Stairs"},
+		MTL{68, 0x7f7f7fff, "Wall Sign"},
+		MTL{69, 0x7f7f7fff, "Lever"},
+		MTL{70, 0x7f7f7fff, "Stone Pressure Plate"},
+		MTL{71, 0x7f7f7fff, "Iron Door"},
+		MTL{72, 0x7f7f7fff, "Wooden Pressure Plate"},
+		MTL{73, 0x7f7f7fff, "Redstone Ore"},
+		MTL{74, 0x7f7f7fff, "Glowing Redstone Ore"},
+		MTL{75, 0x7f7f7fff, "Redstone torch (\"off\" state)"},
+		MTL{76, 0x7f7f7fff, "Redstone torch (\"on\" state)"},
+		MTL{77, 0x7f7f7fff, "Stone Button"},
+		MTL{78, 0xffffffff, "Snow"},
+		MTL{79, 0x70a0ffff, "Ice"},
+		MTL{80, 0xffffffff, "Snow Block"},
+		MTL{81, 0x7f7f7fff, "Cactus"},
+		MTL{82, 0x7f7f7fff, "Clay"},
+		MTL{83, 0x7f7f7fff, "Sugar Cane"},
+		MTL{84, 0x7f7f7fff, "Jukebox"},
+		MTL{85, 0x7f7f7fff, "Fence"},
+		MTL{86, 0x7f7f7fff, "Pumpkin"},
+		MTL{87, 0x7f7f7fff, "Netherrack"},
+		MTL{88, 0x7f7f7fff, "Soul Sand"},
+		MTL{89, 0x7f7f7fff, "Glowstone"},
+		MTL{90, 0x7f7f7fff, "Portal"},
+		MTL{91, 0x7f7f7fff, "Jack-O-Lantern"},
+		MTL{92, 0xffffffff, "Cake Block"},
 	}
 )
 
@@ -730,18 +739,26 @@ func processBlocks(bytes []byte, faces AddFacer) {
 
 			if blocks.IsBoundary(x-1, y, z, blockId) {
 				r1.Update(faces, &blockRun{blockId, Vertex{x, y, z}, Vertex{x, y, z + 1}, Vertex{x, y + 1, z + 1}, Vertex{x, y + 1, z}, true}, true)
+			} else {
+				r1.AddFace(faces)
 			}
 
 			if blocks.IsBoundary(x+1, y, z, blockId) {
 				r2.Update(faces, &blockRun{blockId, Vertex{x + 1, y, z}, Vertex{x + 1, y + 1, z}, Vertex{x + 1, y + 1, z + 1}, Vertex{x + 1, y, z + 1}, true}, false)
+			} else {
+				r2.AddFace(faces)
 			}
 
 			if blocks.IsBoundary(x, y, z-1, blockId) {
 				r3.Update(faces, &blockRun{blockId, Vertex{x, y, z}, Vertex{x, y + 1, z}, Vertex{x + 1, y + 1, z}, Vertex{x + 1, y, z}, true}, false)
+			} else {
+				r3.AddFace(faces)
 			}
 
 			if blocks.IsBoundary(x, y, z+1, blockId) {
 				r4.Update(faces, &blockRun{blockId, Vertex{x, y, z + 1}, Vertex{x + 1, y, z + 1}, Vertex{x + 1, y + 1, z + 1}, Vertex{x, y + 1, z + 1}, true}, true)
+			} else {
+				r4.AddFace(faces)
 			}
 		}
 

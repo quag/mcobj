623c9a43338d467a43d0ffdd132a216fbe228e68
Jonathan Wright <quaggy@gmail.com>
2011-02-07 21:36:15 +1300

    Factor out EnclosedChunk into its own file

---
+++ enclosedChunk.go
@@ -0,0 +1,33 @@
+package main
+
+type EnclosingSides [4]*ChunkSide
+type EnclosedChunk struct {
+	blocks    Blocks
+	enclosing EnclosingSides
+}
+
+func (s *EnclosingSides) side(i int) *ChunkSide {
+	return (*s)[i]
+}
+
+func (e *EnclosedChunk) Get(x, y, z int) (blockId byte) {
+	switch {
+	case y < 0 && hideBottom:
+		blockId = 7 // Bedrock
+	case y < 0 && !hideBottom:
+	case y > 127:
+		blockId = 0
+	case x == -1:
+		blockId = e.enclosing.side(0).BlockId(z, y)
+	case x == 16:
+		blockId = e.enclosing.side(1).BlockId(z, y)
+	case z == -1:
+		blockId = e.enclosing.side(2).BlockId(x, y)
+	case z == 16:
+		blockId = e.enclosing.side(3).BlockId(x, y)
+	default:
+		blockId = e.blocks.Get(x, y, z)
+	}
+
+	return
+}
--- sides.go
+++ sides.go
@@ -13,18 +13,9 @@ func init() {
 
 type ChunkSide [128 * 16]byte
 type ChunkSides [4]ChunkSide
-type EnclosingSides [4]*ChunkSide
-type EnclosedChunk struct {
-	blocks    Blocks
-	enclosing EnclosingSides
-}
 
 func (s *ChunkSides) Side(i int) *ChunkSide {
 	return &((*s)[i])
-}
-
-func (s *EnclosingSides) Side(i int) *ChunkSide {
-	return (*s)[i]
 }
 
 func (s *ChunkSide) Index(x, y int) int {
@@ -43,26 +34,3 @@ func (s *ChunkSide) Column(x int) BlockColumn {
 func (s *ChunkSide) SetBlockId(x, y int, blockId byte) {
 	(*s)[s.Index(x, y)] = blockId
 }
-
-func (e *EnclosedChunk) Get(x, y, z int) (blockId byte) {
-	switch {
-	case y < 0 && hideBottom:
-		blockId = 7 // Bedrock
-	case y < 0 && !hideBottom:
-	case y > 127:
-		blockId = 0
-	case x == -1:
-		blockId = e.enclosing.Side(0).BlockId(z, y)
-	case x == 16:
-		blockId = e.enclosing.Side(1).BlockId(z, y)
-	case z == -1:
-		blockId = e.enclosing.Side(2).BlockId(x, y)
-	case z == 16:
-		blockId = e.enclosing.Side(3).BlockId(x, y)
-	default:
-		blockId = e.blocks.Get(x, y, z)
-	}
-
-	return
-}
-

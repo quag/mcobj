20f58e6cda58cde6f48a2c424701c87396f12981
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2011-05-26 02:16:43 -0700

    finished most of object parsing--still need to figure out material libraries from material name as well as actual flesh and blood material file

--- src/quag.geek.nz/mcobj/objparse.go
+++ src/quag.geek.nz/mcobj/objparse.go
@@ -1,123 +1,196 @@
 package main
+
 import (
-"io"
-"regexp"
-"encoding/line"
-"strconv"
-"fmt"
-"strings"
+	"io"
+	"regexp"
+	"encoding/line"
+	"strconv"
+	"fmt"
+	"strings"
+	"os"
 )
 
 type ObjVec2 struct {
-  x float32;
-  y float32;
+	x float32
+	y float32
 }
 type ObjVec3 struct {
-  x float32;
-  y float32;
-  z float32;
+	x float32
+	y float32
+	z float32
 }
 type ObjReference struct {
-	vertex int;
-	texcoord int;//-1 for missing
-	normal int;// -1 for missing
+	vertex         int
+	texcoord       int //-1 for missing
+	normal         int // -1 for missing
+	materialNumber int
 }
-type ObjFile struct{
-  texcoords []ObjVec2;
-  vertices []ObjVec3;
-  normals []ObjVec3;
-  faces [][]ObjReference;
-  
+type Material struct {
+	texture string
+}
+type ObjFile struct {
+	texcoords []ObjVec2
+	vertices  []ObjVec3
+	normals   []ObjVec3
+	faces     [][]ObjReference
+	materials []Material
 }
 
+var floatingPointNumber string = "([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?)"
+
+func parseMaterialLibrary(mtllib string, materialMap map[string]int, maxMaterial int) (retval []Material) {
+	if maxMaterial == 0 {
+		maxMaterial++
+	}
+	materialRE := regexp.MustCompile("^newmtl[ \t]+([^#]*)")
+	textureRE := regexp.MustCompile("^map_([^# \t]*)[ \t]+([^#]*)")
+	specRE := regexp.MustCompile("^Ns[ \t](" + floatingPointNumber + ")")
+	colorRE := regexp.MustCompile("^(K[a-z])[ \t]+(" + floatingPointNumber + "[ \t]+" + floatingPointNumber + "[ \t]+" + floatingPointNumber + ")")
+	retval = make([]Material, maxMaterial)
+
+	var file, fileErr = os.Open(mtllib)
+	reader := line.NewReader(file, 16384)
+	if fileErr == nil {
+		var dummyMaterial Material
+		curMaterial := &dummyMaterial
+		for linenum := 0; true; linenum++ {
+			line, _, err := reader.ReadLine()
+			if err != nil { //EOF?
+				break
+			}
+			material := materialRE.FindSubmatch(line)
+			texture := textureRE.FindSubmatch(line)
+			spec := specRE.FindSubmatch(line)
+			color := colorRE.FindSubmatch(line)
+			if material != nil {
+				index, ok := materialMap[string(material[1])]
+				if ok {
+					curMaterial = &retval[index]
+				} else {
+					curMaterial = &dummyMaterial
+				}
+			}
+			if texture != nil {
+				curMaterial.texture = string(texture[2]) //FIXME need to look for Kd
+			}
+			if spec != nil {
+
+			}
+			if color != nil {
+
+			}
+		}
+	}
+	return
+}
 func parseObj(f io.Reader) (retval ObjFile) {
-    //integerNumber:="-{0,1}[0-9]+";
-    polygonVertex:="(-{0,1}[0-9]+)(/-{0,1}[0-9]*)?(/-{0,1}[0-9]*)?"
-    //materialLibRE:=regexp.MustCompile("^mtllib[ \t]+([^#]*)");
-    floatingPointNumber:="([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?)";
-    //usemtlRE:=regexp.MustCompile("^usemtl[ \t]+([^#]*)");
-    vertexRE:=regexp.MustCompile("^v[ \t]+"+floatingPointNumber+"[ \t]+"+floatingPointNumber+"[ \t]+"+floatingPointNumber);
-    normalRE:=regexp.MustCompile("^vn[ \t]+"+floatingPointNumber+"[ \t]+"+floatingPointNumber+"[ \t]+"+floatingPointNumber);
-    texCoordRE:=regexp.MustCompile("^vt[ \t]+"+floatingPointNumber+"[ \t]+"+floatingPointNumber);
-    polygonRE:=regexp.MustCompile("^f[ \t]+("+polygonVertex+"([ \t]+"+polygonVertex+")*"+")");
-    reader :=line.NewReader(f,16384);
-    for linenum:=0;true;linenum++ {
-        line,_,err:=reader.ReadLine()
-        if err!=nil {//EOF?
-            break;
-        }
-        vertex:=vertexRE.FindSubmatch(line)
-        normal:=normalRE.FindSubmatch(line)
-        texCoord:=texCoordRE.FindSubmatch(line)
-        polygon:=polygonRE.FindSubmatch(line)
-        if (vertex!=nil) {
-			x,err0:=strconv.Atof32(string(vertex[1]))
-			y,err1:=strconv.Atof32(string(vertex[2]))
-			z,err2:=strconv.Atof32(string(vertex[3]))
-			if (err0!=nil||err1!=nil||err2!=nil) {
-				fmt.Println("Error parsing obj vertex file, line: ",linenum,err0,err1,err2);
+	curMaterial := 0
+	maxMaterial := 0
+	var materialMap map[string]int
+	//integerNumber:="-{0,1}[0-9]+";
+	polygonVertex := "(-{0,1}[0-9]+)(/-{0,1}[0-9]*)?(/-{0,1}[0-9]*)?"
+	mtllibRE := regexp.MustCompile("^mtllib[ \t]+([^#]*)")
+	usemtlRE := regexp.MustCompile("^usemtl[ \t]+([^#]*)")
+	vertexRE := regexp.MustCompile("^v[ \t]+" + floatingPointNumber + "[ \t]+" + floatingPointNumber + "[ \t]+" + floatingPointNumber)
+	normalRE := regexp.MustCompile("^vn[ \t]+" + floatingPointNumber + "[ \t]+" + floatingPointNumber + "[ \t]+" + floatingPointNumber)
+	texCoordRE := regexp.MustCompile("^vt[ \t]+" + floatingPointNumber + "[ \t]+" + floatingPointNumber)
+	polygonRE := regexp.MustCompile("^f[ \t]+(" + polygonVertex + "([ \t]+" + polygonVertex + ")*" + ")")
+	reader := line.NewReader(f, 16384)
+	var materialFileName string
+	for linenum := 0; true; linenum++ {
+		line, _, err := reader.ReadLine()
+		if err != nil { //EOF?
+			break
+		}
+		vertex := vertexRE.FindSubmatch(line)
+		normal := normalRE.FindSubmatch(line)
+		texCoord := texCoordRE.FindSubmatch(line)
+		polygon := polygonRE.FindSubmatch(line)
+		usemtl := usemtlRE.FindSubmatch(line)
+		mtllib := mtllibRE.FindSubmatch(line)
+		if mtllib != nil {
+			materialFileName = string(mtllib[1])
+
+		}
+		if usemtl != nil {
+			desiredMaterialName := string(usemtl[1])
+			desiredMaterialNumber, ok := materialMap[desiredMaterialName]
+			if !ok {
+				desiredMaterialNumber = maxMaterial
+				maxMaterial++
+				materialMap[desiredMaterialName] = desiredMaterialNumber
 			}
-            retval.vertices=append(retval.vertices,ObjVec3{x,y,z});
-        }
-        if (normal!=nil) {
-			x,err0:=strconv.Atof32(string(normal[1]))
-			y,err1:=strconv.Atof32(string(normal[2]))
-			z,err2:=strconv.Atof32(string(normal[3]))
-			if (err0!=nil||err1!=nil||err2!=nil) {
-				fmt.Println("Error parsing obj normal file, line: ",linenum,err0,err1,err2);
+			curMaterial = desiredMaterialNumber
+		}
+		if vertex != nil {
+			x, err0 := strconv.Atof32(string(vertex[1]))
+			y, err1 := strconv.Atof32(string(vertex[2]))
+			z, err2 := strconv.Atof32(string(vertex[3]))
+			if err0 != nil || err1 != nil || err2 != nil {
+				fmt.Println("Error parsing obj vertex file, line: ", linenum, err0, err1, err2)
 			}
-            retval.normals=append(retval.normals,ObjVec3{x,y,z});
-        }
-        if (texCoord!=nil) {
-			u,err0:=strconv.Atof32(string(texCoord[1]))
-			v,err1:=strconv.Atof32(string(texCoord[2]))
-			if (err0!=nil||err1!=nil) {
-				fmt.Println("Error parsing obj texcoord file, line: ",linenum,err0,err1);
+			retval.vertices = append(retval.vertices, ObjVec3{x, y, z})
+		}
+		if normal != nil {
+			x, err0 := strconv.Atof32(string(normal[1]))
+			y, err1 := strconv.Atof32(string(normal[2]))
+			z, err2 := strconv.Atof32(string(normal[3]))
+			if err0 != nil || err1 != nil || err2 != nil {
+				fmt.Println("Error parsing obj normal file, line: ", linenum, err0, err1, err2)
 			}
-            retval.texcoords=append(retval.texcoords,ObjVec2{u,v});              
-        }
-        if (polygon!=nil) {
-			faces := strings.Split(strings.Replace(string(polygon[1]),"\t"," ",-1)," ",-1);
-			face := make ([]ObjReference,0);
-			for _,facestr := range faces {
-				references:=strings.Split(facestr,"/",3);
-				if (len(references)!=0) {
-					v,errv:=strconv.Atoi(references[0])
-					if (errv==nil) {
-						curFaceVertex:=ObjReference{-1,-1,-1};
-						if (v<0){
-							curFaceVertex.vertex=len(retval.vertices)+v;
-						}else {
-							curFaceVertex.vertex=v;
+			retval.normals = append(retval.normals, ObjVec3{x, y, z})
+		}
+		if texCoord != nil {
+			u, err0 := strconv.Atof32(string(texCoord[1]))
+			v, err1 := strconv.Atof32(string(texCoord[2]))
+			if err0 != nil || err1 != nil {
+				fmt.Println("Error parsing obj texcoord file, line: ", linenum, err0, err1)
+			}
+			retval.texcoords = append(retval.texcoords, ObjVec2{u, v})
+		}
+		if polygon != nil {
+			faces := strings.Split(strings.Replace(string(polygon[1]), "\t", " ", -1), " ", -1)
+			face := make([]ObjReference, 0)
+			for _, facestr := range faces {
+				references := strings.Split(facestr, "/", 3)
+				if len(references) != 0 {
+					v, errv := strconv.Atoi(references[0])
+					if errv == nil {
+						curFaceVertex := ObjReference{-1, -1, -1, curMaterial}
+						if v < 0 {
+							curFaceVertex.vertex = len(retval.vertices) + v
+						} else {
+							curFaceVertex.vertex = v
 						}
-						if (len(references)>1) {
-							t,errt:=strconv.Atoi(references[1])
-							if (errt==nil) {
-								if (t<0){
-									curFaceVertex.texcoord=len(retval.texcoords)+t;
-								}else {
-									curFaceVertex.texcoord=t;
+						if len(references) > 1 {
+							t, errt := strconv.Atoi(references[1])
+							if errt == nil {
+								if t < 0 {
+									curFaceVertex.texcoord = len(retval.texcoords) + t
+								} else {
+									curFaceVertex.texcoord = t
 								}
 							}
 						}
-						if (len(references)>2) {
-							n,errn:=strconv.Atoi(references[2])
-							if (errn==nil) {
-								if (n<0){
-									curFaceVertex.normal=len(retval.normals)+n;
-								}else {
-									curFaceVertex.normal=n;
+						if len(references) > 2 {
+							n, errn := strconv.Atoi(references[2])
+							if errn == nil {
+								if n < 0 {
+									curFaceVertex.normal = len(retval.normals) + n
+								} else {
+									curFaceVertex.normal = n
 								}
 							}
 						}
-						face=append(face,curFaceVertex);              
+						face = append(face, curFaceVertex)
 					}
 				}
-            }
-			if (len(face)>2) {
-				retval.faces=append(retval.faces,face);
 			}
-        }
-    }
-	return;
+			if len(face) > 2 {
+				retval.faces = append(retval.faces, face)
+			}
+		}
+	}
+	retval.materials = parseMaterialLibrary(materialFileName, materialMap, maxMaterial)
+	return
 }
